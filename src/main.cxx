39965ad3aa97fdfd203944009fcfce462c1320c6
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2019-05-21 11:25:09 -0400

    works now

--- src/main.cxx
+++ src/main.cxx
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "qapplication.h"
 #include "qoffscreensurface.h"
 #include "qopenglcontext.h"
@@ -6,7 +8,7 @@
 
 int main(int argc, char** argv)
 {
-  QApplication app(argc, argv);
+  QApplication app(argc, argv);
 
   QOffscreenSurface surf;
   surf.create();
@@ -14,10 +16,13 @@ int main(int argc, char** argv)
   QOpenGLContext ctx;
   ctx.create();
   ctx.makeCurrent(&surf);
+  
+  std::string gl_version = reinterpret_cast<const char*>(ctx.functions()->glGetString(GL_VERSION));
+  std::string gl_extensions = reinterpret_cast<const char*>(ctx.functions()->glGetString(GL_EXTENSIONS));
 
-  qDebug() << (const char*)ctx.functions()->glGetString(GL_VERSION);
-  qDebug() << (const char*)ctx.functions()->glGetString(GL_EXTENSIONS);
-
+  std::cout << "OpenGL Version: " << gl_version << "\n===\n";
+  std::cout << "OpenGL Extensions: " << gl_extensions << "\n";
+  
   return 0;
 
 }
